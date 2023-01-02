b0e4f2587735b2a09fc3f264ba5cf37ad5e9ce1f
leedaehun <necromman@gmail.com>
2023-01-02 21:45:27 +0900

    s

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    proxy: "http://localhost:3000",
-  }
-})
+const target = 'http://localhost:3000';
+
+module.exports={
+    devServer: {
+        port: 8080,
+        proxy: {
+            //proxy 요청을 보낼 api 시작 부분
+            '^/api': {
+                target,
+                changeOrigin: true
+            }
+        }
+    }
+}
