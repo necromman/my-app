3598b431140aca3c92d50d980836ed7eb08a2846
necromman <necromman@gmail.com>
2023-03-15 16:12:59 +0900

    s

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -688,16 +688,13 @@ export default {
         this.$store.state.voCumns[index].req = []
         this.$store.state.voCumns[index].res = []
         console.log("응답 데이터 파라미터: " + JSON.stringify(response.data));
-        console.log("응답 데이터 파라미터 length: " + JSON.stringify(response.data.length));
-        response.data.forEach((column, sindex) => {
-            column.requests.forEach((column, sindex) => {
-              tempList.push(column)
-              this.$store.state.voCumns[index].req.push(column)
-            })
-            column.responses.forEach((column, sindex) => {
-              tempList.push(column)
-              this.$store.state.voCumns[index].res.push(column)
-            })
+        response.data.requests.forEach((column, sindex) => {
+            tempList.push(column)
+            this.$store.state.voCumns[index].req.push(column)
+          })
+        response.data.responses.forEach((column, sindex) => {
+            tempList.push(column)
+            this.$store.state.voCumns[index].res.push(column)
           })
           let tempListSet = [...new Set(tempList)];
           this.$store.state.voCumns[index].columns = []
